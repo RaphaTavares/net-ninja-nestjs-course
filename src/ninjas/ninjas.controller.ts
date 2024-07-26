e1112e95dfc41361266a6038fe01559e6d64b6ee
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-07-25 22:15:24 -0300

    feat: implement controllers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    
+    // GET /ninjas --> []
+    // GET /ninjas/:id --> { ... }
+    // POST /ninjas
+    // PUT /ninjas/:id --> { ... }
+    // DELETE /ninjas/:id,
+    
+    @Get()
+    getNinjas(@Query('type') type: string) {
+        return[{ type }];
+    }
+
+    @Get(':id')
+    getNinjaById(@Param('id') id: string){
+        return { id };
+    }
+
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+        return {
+            name: createNinjaDto.name
+        };
+    }
+
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return {};
+    }
+
+    @Delete(':id')
+    deleteNinja(@Param('id') id: string){
+        return {};
+    }
+
+}
